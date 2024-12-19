86c0c5cc97dd6e1259c47b7ca14ae85a4bccf516
danieltsega <danieltsega6658@gmail.com>
2024-12-19 15:59:00 +0300

    feat: add home view to the menu

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Food
 
-# Create your views here.
+def home(request):
+    categories = Category.objects.prefetch_related('foods').all()
+    return render(request, 'menu/home.html', {'categories': categories})
